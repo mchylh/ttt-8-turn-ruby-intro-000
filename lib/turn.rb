0986354b566a164216deb5e3bb1625895d8294a1
Michaylah Mam <mchylh@gmail.com>
2020-02-22 01:18:44 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -18,3 +18,8 @@ def display_board(board)
    puts "-----------"
    puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+#change player input in to integer
+def input_to_index(user_input)
+   user_input.to_i - 1
+end
