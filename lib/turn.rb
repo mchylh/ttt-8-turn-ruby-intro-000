f6fa0b0860721022de11acfb3d710c611ae4945c
Michaylah Mam <mchylh@gmail.com>
2020-02-22 01:05:31 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,19 +7,13 @@ def display_board(board)
   puts "   |   |   "
 end
 
-#make move on board
-def move(board, index, token)
-  board[index] = token
-  play(board)
-end
-
-#change input to integer
+#CHANGE INFO TO INTEGER
 def input_to_index(move)
    index = move.to_i - 1
    index
 end
 
-#turn(board)
+#MAKE TURN
 def turn(board)
   puts "Please enter 1-9:"
   #get the user input
@@ -27,25 +21,25 @@ def turn(board)
   #input to index
   index = input_to_index(user_input)
   token = current_player(board)
-
+  
   #check for validation
   if valid_move?(board,index)
     puts 'valid move'
     move(board, index, token)
     display_board(board)
    else
-    puts 'invalid move'
+    puts 'try again'
     turn(board)
   end
   display_board(board)
 end
 
-#position_taken?
+#CHECK FOR EMPTY SPACE
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-#valid_move?
+#IS # CORRECT & SPACE EMPTY?
 def valid_move?(board, index)
 
   if index.between?(0,8) && !position_taken?(board, index)
@@ -54,4 +48,33 @@ def valid_move?(board, index)
   else
    return false
   end
+end
+
+#COUNTER FOR PLAYER ASSESMENT
+def turn_count(board)
+  counter = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      counter +=1
+  end
+end
+return counter
+end
+
+#WHICH PLAYER IS THIS?
+def current_player(board)
+  if turn_count(board)%2 ==0
+    current_player = "X"
+  else
+    current_player = "O"
+end
+return current_player
+end
+
+
+#MAKE MOVE ON BOARD
+def move(board, index, token)
+  board[index] = token
+  play(board)
+  
 end
