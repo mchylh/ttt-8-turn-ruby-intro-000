f4777a168dcc71beee63b2872295ce52e17321b8
Michaylah Mam <mchylh@gmail.com>
2020-02-22 00:54:15 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -30,7 +30,29 @@ def move(board, index, token)
 
 end
 
+#change input to integer
+def input_to_index(move)
+   index = move.to_i - 1
+   index
+end
+
 #turn(board)
 def turn(board)
   puts "Please enter 1-9:"
+  #get the user input
+  user_input = gets.strip
+  #input to index
+  index = input_to_index(user_input)
+  token = current_player(board)
+  
+  #check for validation
+  if valid_move?(board,index)
+    puts 'valid move'
+    move(board, index, token)
+    display_board(board)
+   else
+    puts 'try again'
+    turn(board)
+  end
+  display_board(board)
 end
