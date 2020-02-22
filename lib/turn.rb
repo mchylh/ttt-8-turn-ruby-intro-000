4eda7f93f3491dbb09e89d0c2b3d38d1ced37a09
Michaylah Mam <mchylh@gmail.com>
2020-02-22 01:35:13 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -23,3 +23,65 @@ end
 def input_to_index(user_input)
    user_input.to_i - 1
 end
+
+#MAKE TURN
+def turn(board)
+  puts "Please enter 1-9:"
+  #get the user input
+  user_input = gets.strip
+  #input to index
+  index = input_to_index(user_input)
+  token = current_player(board)
+
+  #check for validation
+  if valid_move?(board,index)
+    puts 'valid move'
+    move(board, index, token)
+    display_board(board)
+   else
+    puts 'try again'
+    turn(board)
+  end
+  display_board(board)
+end
+
+#move
+def move(board, index, player)
+   board[index] = player
+end
+
+# code your valid_move? method here
+def valid_move?(board, index)
+   if index.between?(0,8) && !position_taken?(board, index)
+        puts "this is a valid move"
+      return true
+    else
+      return false
+   end
+end
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken? (board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+#MAKE TURN
+def turn(board)
+  puts "Please enter 1-9:"
+  #get the user input
+  user_input = gets.strip
+  #input to index
+  index = input_to_index(user_input)
+  token = current_player(board)
+
+  #check for validation
+  if valid_move?(board,index)
+    puts 'valid move'
+    move(board, index, token)
+    display_board(board)
+   else
+    puts 'try again'
+    turn(board)
+  end
+  display_board(board)
+end
